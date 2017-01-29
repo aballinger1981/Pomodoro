d6364b1ca088034fa84a92627d4c4ecdc0214a0e
aballinger1981 <andrew@andrewballinger.com>
2017-01-28 20:13:56 -0500

    added components and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,26 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { TimeService } from './time.service';
+import { HourglassComponent } from './hourglass/hourglass.component';
+import { ClockComponent } from './clock/clock.component';
+import { StartRestartComponent } from './start-restart/start-restart.component';
+import { TimerSettingsComponent } from './timer-settings/timer-settings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HourglassComponent,
+    ClockComponent,
+    StartRestartComponent,
+    TimerSettingsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [TimeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
