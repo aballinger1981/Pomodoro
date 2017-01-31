527d4eaff863a5a115c8eb26dabc19950a944cf9
jacktho <jack@viosolutions.net>
2017-01-31 14:55:17 -0500

    initial bottom sand animation

--- src/app/hourglass/hourglass.component.ts
+++ src/app/hourglass/hourglass.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { TimeService } from '../time.service';
 
 @Component({
@@ -7,10 +7,21 @@ import { TimeService } from '../time.service';
   styleUrls: ['./hourglass.component.css']
 })
 export class HourglassComponent implements OnInit {
+  @ViewChild('bottomSand') bottomSand: ElementRef;
+  @ViewChild('topTriangle') topTriangle: ElementRef;
+  @ViewChild('bottomTriangle') bottomTriangle: ElementRef;
+  @ViewChild('innerRectangle') innerRectangle: ElementRef;
 
   constructor(private timeService: TimeService) { }
 
   ngOnInit() {
+    TweenMax.to(this.bottomSand.nativeElement, 60, {
+      attr: {
+        points: '4,260.3 50,180 77,150.9 104,180 150,260.3'
+      },
+      repeat: -1,
+      yoyo: true,
+      // ease: Cubic.easeInOut
+    });
   }
-
 }
